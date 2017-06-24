3d20e1b4c89775a982b2df454bd2a884ccdb0c6c
al-metal <al-metal@users.noreply.github.com>
2017-06-24 11:48:11 +0400

    Рабочая программа
    Получение всех товаров

--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,6 +17,7 @@ namespace allTovarBike18
     {
         Thread forms;
         CookieDictionary cookieNethouse = new CookieDictionary();
+        int countTovars = 0;
 
         public Form1()
         {
@@ -36,6 +38,8 @@ namespace allTovarBike18
             Properties.Settings.Default.Save();
             #endregion
 
+            countTovars = 0;
+
             Thread tabl = new Thread(() => Actual());
             forms = tabl;
             forms.IsBackground = true;
@@ -44,27 +48,92 @@ namespace allTovarBike18
 
         private void Actual()
         {
-            using (var request = new HttpRequest())
+            string otv = GetRequest("https://bike18.ru/products");
+            MatchCollection razdel = new Regex("(?<=<div class=\"category-capt-txt -text-center\"><a href=\").*?(?=\" class=\"blue\">)").Matches(otv);
+            MatchCollection tovar = new Regex("(?<=<div class=\"product-link -text-center\"><a href=\").*?(?=\" >)").Matches(otv);
+            if (tovar.Count != 0)
+                countTovars += tovar.Count;
+            else
             {
-                request.UserAgent = HttpHelper.RandomChromeUserAgent();
-                request.Cookies = cookieNethouse;
-                request.Proxy = HttpProxyClient.Parse("127.0.0.1:8888");
-                string post_data = "login=" + tbLogin.Text + "&password=" + tbPassword.Text + "&quick_expire=0&submit=%D0%92%D0%BE%D0%B9%D1%82%D0%B8";
-                request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36";
-                byte[] dataStream = Encoding.UTF8.GetBytes(String.Format(post_data));
-                request.Post("https://nethouse.ru/signin", dataStream).ToString();
-            }
-            if (cookieNethouse.Count == 1)
-            {
-                MessageBox.Show("Логин или пароль для сайта Nethouse введены не верно", "Ошибка логина/пароля");
-                return;
+                for (int i = 0; razdel.Count > i; i++)
+                {
+                    string urlRazdel = razdel[i].ToString();
+                    otv = GetRequest("https://bike18.ru" + urlRazdel + "?page=all");
+                    MatchCollection razdel2 = new Regex("(?<=<div class=\"category-capt-txt -text-center\"><a href=\").*?(?=\" class=\"blue\">)").Matches(otv);
+                    tovar = new Regex("(?<=<div class=\"product-link -text-center\"><a href=\").*?(?=\" >)").Matches(otv);
+                    if (tovar.Count != 0)
+                        countTovars += tovar.Count;
+                    else
+                    {
+                        for (int i2 = 0; razdel2.Count > i2; i2++)
+                        {
+                            string urlRazdel2 = razdel2[i2].ToString();
+                            otv = GetRequest("https://bike18.ru" + urlRazdel2 + "?page=all");
+                            MatchCollection razdel3 = new Regex("(?<=<div class=\"category-capt-txt -text-center\"><a href=\").*?(?=\" class=\"blue\">)").Matches(otv);
+                            tovar = new Regex("(?<=<div class=\"product-link -text-center\"><a href=\").*?(?=\" >)").Matches(otv);
+                            if (tovar.Count != 0)
+                                countTovars += tovar.Count;
+                            else
+                            {
+                                for (int i3 = 0; razdel3.Count > i3; i3++)
+                                {
+                                    string urlRazdel3 = razdel3[i3].ToString();
+                                    otv = GetRequest("https://bike18.ru" + urlRazdel3 + "?page=all");
+                                    MatchCollection razdel4 = new Regex("(?<=<div class=\"category-capt-txt -text-center\"><a href=\").*?(?=\" class=\"blue\">)").Matches(otv);
+                                    tovar = new Regex("(?<=<div class=\"product-link -text-center\"><a href=\").*?(?=\" >)").Matches(otv);
+                                    if (tovar.Count != 0)
+                                        countTovars += tovar.Count;
+                                    else
+                                    {
+                                        for (int i4 = 0; razdel4.Count > i4; i4++)
+                                        {
+                                            string urlRazdel4 = razdel4[i4].ToString();
+                                            otv = GetRequest("https://bike18.ru" + urlRazdel4 + "?page=all");
+                                            MatchCollection razdel5 = new Regex("(?<=<div class=\"category-capt-txt -text-center\"><a href=\").*?(?=\" class=\"blue\">)").Matches(otv);
+                                            tovar = new Regex("(?<=<div class=\"product-link -text-center\"><a href=\").*?(?=\" >)").Matches(otv);
+                                            if (tovar.Count != 0)
+                                                countTovars += tovar.Count;
+                                            else
+                                            {
+                                                for (int i5 = 0; razdel5.Count > i5; i5++)
+                                                {
+                                                    string urlRazdel5 = razdel5[i5].ToString();
+                                                    otv = GetRequest("https://bike18.ru" + urlRazdel5 + "?page=all");
+                                                    MatchCollection razdel6 = new Regex("(?<=<div class=\"category-capt-txt -text-center\"><a href=\").*?(?=\" class=\"blue\">)").Matches(otv);
+                                                    tovar = new Regex("(?<=<div class=\"product-link -text-center\"><a href=\").*?(?=\" >)").Matches(otv);
+                                                    if (tovar.Count != 0)
+                                                        countTovars += tovar.Count;
+                                                    else
+                                                    {
+                                                        for (int i6 = 0; razdel6.Count > i6; i6++)
+                                                        {
+                                                            string urlRazdel6 = razdel6[i6].ToString();
+                                                            otv = GetRequest("https://bike18.ru" + urlRazdel6 + "?page=all");
+                                                            MatchCollection razdel7 = new Regex("(?<=<div class=\"category-capt-txt -text-center\"><a href=\").*?(?=\" class=\"blue\">)").Matches(otv);
+                                                            tovar = new Regex("(?<=<div class=\"product-link -text-center\"><a href=\").*?(?=\" >)").Matches(otv);
+                                                            if (tovar.Count != 0)
+                                                                countTovars += tovar.Count;
+                                                            else
+                                                            {
+
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
 
-            string otv = GetRequest("");
+            MessageBox.Show("Количество товаров = " + countTovars);
 
 
         }
-
         private string GetRequest(string v)
         {
             var request2 = new HttpRequest();
@@ -76,6 +145,5 @@ namespace allTovarBike18
             string content = response.ToText();
             return content;
         }
-        
     }
 }
