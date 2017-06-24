e1a4f5a4925cbd889571020a13fcef378cc4f43d
al-metal <al-metal@users.noreply.github.com>
2017-06-24 10:20:05 +0400

    Авторизация нетхоусе

--- Form1.cs
+++ Form1.cs
@@ -5,13 +5,18 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using xNet.Net;
 
 namespace allTovarBike18
 {
     public partial class Form1 : Form
     {
+        Thread forms;
+        CookieDictionary cookieNethouse = new CookieDictionary();
+
         public Form1()
         {
             InitializeComponent();
@@ -30,6 +35,47 @@ namespace allTovarBike18
             Properties.Settings.Default.password = tbPassword.Text;
             Properties.Settings.Default.Save();
             #endregion
+
+            Thread tabl = new Thread(() => Actual());
+            forms = tabl;
+            forms.IsBackground = true;
+            forms.Start();
         }
+
+        private void Actual()
+        {
+            using (var request = new HttpRequest())
+            {
+                request.UserAgent = HttpHelper.RandomChromeUserAgent();
+                request.Cookies = cookieNethouse;
+                request.Proxy = HttpProxyClient.Parse("127.0.0.1:8888");
+                string post_data = "login=" + tbLogin.Text + "&password=" + tbPassword.Text + "&quick_expire=0&submit=%D0%92%D0%BE%D0%B9%D1%82%D0%B8";
+                request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36";
+                byte[] dataStream = Encoding.UTF8.GetBytes(String.Format(post_data));
+                request.Post("https://nethouse.ru/signin", dataStream).ToString();
+            }
+            if (cookieNethouse.Count == 1)
+            {
+                MessageBox.Show("Логин или пароль для сайта Nethouse введены не верно", "Ошибка логина/пароля");
+                return;
+            }
+
+            string otv = GetRequest("");
+
+
+        }
+
+        private string GetRequest(string v)
+        {
+            var request2 = new HttpRequest();
+            request2.UserAgent = HttpHelper.RandomChromeUserAgent();
+            request2.Proxy = HttpProxyClient.Parse("127.0.0.1:8888");
+            // Отправляем запрос.
+            HttpResponse response = request2.Get(v);
+            // Принимаем тело сообщения в виде строки.
+            string content = response.ToText();
+            return content;
+        }
+        
     }
 }
